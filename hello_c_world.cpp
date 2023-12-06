c3bfd76b6e715af70368e660d9b73ffc4b6a6276
ptcriuer <su7807@yahoo.com.tw>
2023-12-06 22:13:16 +0800

    delete

--- hello_c_world.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(){
-	
-	printf("hello\"C\"world!");
-	
-	return 0;
-}
--- sizeof.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(){
-    long int a = 32;
-    printf("long int=%d",sizeof(long int));
-
-    return 0;
-}
